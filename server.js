09c4ad73dbbaaa58f086b98f5d861b6666451db8
vanquang2002 <132914620+vanquang2002@users.noreply.github.com>
2024-12-07 22:26:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ const server = http.createServer(app);
 app.use(json());
 app.use(
   cors({
-    origin: process.env.REACT_URL || "http://localhost:3000", // Đường dẫn frontend
+    origin: process.env.REACT_URL || "http://localhost:3001", // Đường dẫn frontend
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Hỗ trợ cookie (nếu cần)
@@ -51,7 +51,7 @@ app.use(
 // Socket.io setup
 const io = new Server(server, {
   cors: {
-    origin: process.env.REACT_URL || "http://localhost:3000",
+    origin: process.env.REACT_URL || "http://localhost:3001",
     methods: ["GET", "POST"],
     credentials: true,
   },
